d152dacebb8f0ea03d90bbd271378e697e55d9e2
narsimha-na <activena1@gmail.com>
2021-12-06 19:14:19 +0530

    Profile Update

--- src/portfolio.js
+++ src/portfolio.js
@@ -174,7 +174,7 @@ export const projects = [{
     {
         name: "MAVEX",
         desc: "MaveX app lets you book tickets to all those huge and tiny events happening around you within seconds. It brings events related to music, sports, arts, technology, and a lot more to you from around the country. \n I have developed android applicaiton,Web applicaitons & API services for this start-up",
-        github: "https://github.com/1hanzla100",
+        github: "https://github.com/narsimha-na",
         link: "https://play.google.com/store/apps/details?id=com.maverick.user"
     },
     {
@@ -183,12 +183,4 @@ export const projects = [{
     },
 ];
 
-export const feedbacks = [{
-        name: "Hassan Mehmood",
-        feedback: "We have been very happy with our new website! It looks professional and very easy to navigate. Our experience with the customer service at Hanzla tauqeer has been great. They handle things very efficiently and are available for any questions we have. They also keep us updated on daily reports so we know how our site is doing. I would recommend that u have choose hanzla web developer services for u.",
-    },
-    {
-        name: "Maheen Altaf",
-        feedback: "the website is very nice, everything was perfectly made as i need. it just loads in moments, without giving u the chance to wait. the quality is also very amazing. i am using it without any problem. great job",
-    },
-];
+export const feedbacks = [];
